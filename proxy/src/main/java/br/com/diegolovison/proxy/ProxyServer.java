1101638d0ab31a4f0c5b09951a228023a7cc4b2d
Diego Lovison <diegolovison@gmail.com>
2015-11-10 11:31:45 -0200

    oauth to resource

--- proxy/src/main/java/br/com/diegolovison/proxy/ProxyServer.java
+++ proxy/src/main/java/br/com/diegolovison/proxy/ProxyServer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 /**
  * Created by diego.lovison on 16/10/2015.
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableOAuth2Resource
 public class ProxyServer {
 
     public static void main(String... args) {
